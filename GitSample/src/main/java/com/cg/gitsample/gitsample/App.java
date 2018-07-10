9486dbad5bb6eba4cb54b59b07246cf50a8aa961
learning <learning@DIN66008879.corp.capgemini.com>
2018-07-10 10:35:44 +0530

    changes

--- GitSample/src/main/java/com/cg/gitsample/gitsample/App.java
+++ GitSample/src/main/java/com/cg/gitsample/gitsample/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Venkata Praveena Neti");
+        System.out.println("Venkata Praveena Neti2");
     }
 }
