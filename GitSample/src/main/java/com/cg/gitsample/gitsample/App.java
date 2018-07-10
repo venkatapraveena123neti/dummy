0799b4ceb4d7f958dba42b69d99b9084c4bbdd36
learning <learning@DIN66008879.corp.capgemini.com>
2018-07-10 10:22:56 +0530

    version 1.0 veena_repository

---
+++ GitSample/src/main/java/com/cg/gitsample/gitsample/App.java
@@ -0,0 +1,14 @@
+package com.cg.gitsample.gitsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Venkata Praveena Neti");
+    }
+}
